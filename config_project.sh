13f0a356613857d5ae11feafbba347c0db1a48cd
Jeff Hollan <jeff.hollan@snowflake.com>
2023-10-01 17:57:25 -0700

    fixing merge

--- config_project.sh
+++ config_project.sh
@@ -11,20 +11,11 @@ read -p "What is your HuggingFace token? " hf_token
 llm_file="./LLM/llm.yaml"
 streamlit_file="./streamlit/streamlit.yaml"
 
-<<<<<<< HEAD
 # Replace placeholders in LLM file using | as delimiter
 sed -i "" "s|<<repository_url>>|$repository_url|g" $llm_file
 sed -i "" "s|<<HF_TOKEN>>|$hf_token|g" $llm_file
 sed -i "" "s|<<database>>|$database|g" $llm_file
 sed -i "" "s|<<schema>>|$schema|g" $llm_file
-=======
-# Replace placeholders in LLM file
-sed -i "s/<<repository_url>>/$repository_url/g" $llm_file
-sed -i "s/<<HF_USERNAME>>/$hf_username/g" $llm_file
-sed -i "s/<<HF_TOKEN>>/$hf_token/g" $llm_file
-sed -i "s/<<database>>/$database/g" $llm_file
-sed -i "s/<<schema>>/$schema/g" $llm_file
->>>>>>> 2dad4c0db1af9c1905154dc9b3e7f0f3421f5e80
 
 # Replace placeholders in Streamlit file using | as delimiter
 sed -i "" "s|<<repository_url>>|$repository_url|g" $streamlit_file
